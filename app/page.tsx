95b4ccc4d6e2d35cce639f40822daae2325ec57c
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-07 16:15:34 +0800

    root?

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {title: "5bus"};
-export default function R({ children }: {children: React.ReactNode}) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
   return (
-    <html>
+    <html lang="en">
       <body>{children}</body>
     </html>
-  );
+  )
 }
