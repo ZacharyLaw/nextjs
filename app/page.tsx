e7dcd0cd807ce61ed9a9d700bf1bfee9453eb062
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-07 16:13:08 +0800

    from exp

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {title: "5bus"};
-export default function R({ children }: Readonly<{ children: ReactNode }>) {
+export default function R({ children }: {children: React.ReactNode}) {
   return (
     <html>
       <body>{children}</body>
