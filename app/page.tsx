8764d080f380a3164f197af39b621c825b39ee89
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-07 16:02:35 +0800

    Risk it?

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
 export const metadata: Metadata = {title: "5bus"};
-export default function RootLayout({children,
-}: Readonly<{children: React.ReactNode}>) {
-  return (<html><body>{children}</body></html>);
+export default function RootLayout({ children }: Readonly<{ children: ReactNode }>) {
+  return (
+    <html>
+      <body>{children}</body>
+    </html>
+  );
 }
