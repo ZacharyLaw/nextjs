8d651a065859cd8e74e77b8f807e5b843d183227
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-16 10:05:09 +0800

    pwa?

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ export default function Home() {
     <div className={styles.container}>
       <main className={styles.main}>
         <h1>yolo</h1>
-      <iframe src="https://web.chelaile.net.cn/customer_ch5/?1=2&src=webapp_zhongshan&sessionId=CB9EF8D44B34EF63E1FBD2F362776C1C#!/linedetail/0760-018-0"></iframe>
+      
       </main>
     </div>
   );
 }
+
+/** <iframe src="https://web.chelaile.net.cn/customer_ch5/?1=2&src=webapp_zhongshan&sessionId=CB9EF8D44B34EF63E1FBD2F362776C1C#!/linedetail/0760-018-0"></iframe>*/
