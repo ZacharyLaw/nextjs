96b85a4318b9cfbac9998757aa5fe093997472cc
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-19 08:31:21 +0800

    cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { NextApiRequest, NextApiResponse } from "next";
-
+//https://5buspwc.vercel.app/api/hello
 const hello = (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: "John Doe" });
 };
