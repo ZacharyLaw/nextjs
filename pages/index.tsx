c585d2c2acefff9f8d081d79f35e8bee03710caf
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-19 08:33:06 +0800

    cors

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1>yolo</h1>
+        <h1>yolo cors</h1>
       
       </main>
     </div>
