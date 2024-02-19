bd0d7b394b06da6d0891ce162a49516fcc197f37
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-19 10:06:23 +0800

    load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,18 @@
 'use client'
-
 import styles from "../styles/Home.module.css";
 import { useLocalStorage } from "../components/useLocalStorage.js";
+import $ from 'jquery';
+import { useState, useEffect } from "react";
+
 export default function Home() {
+  useEffect(() => {
+    $(document).ready(function() {
+      console.log("jQuery is working!");
+      console.log($('.lane'))
+      $('.lane').load('https://web.chelaile.net.cn/customer_ch5/?1=2&src=webapp_zhongshan#!/linedetail/0760-018-0');
+
+    });
+  }, []);
   const [count, setCount] = useLocalStorage("count", 0);
   return (
     <div className={styles.container}>
@@ -10,10 +20,9 @@ export default function Home() {
         <h1>yolo cors {count}</h1>
         <button onClick={()=>setCount(count-1)}>Decrement</button>
         <button onClick={()=>setCount(count+1)}>Increment</button>
+        <aside class='lane'></aside>
         <iframe src="https://web.chelaile.net.cn/customer_ch5/?1=2&src=webapp_zhongshan#!/linedetail/0760-018-0"></iframe>
       </main>
     </div>
   );
 }
-
-/** */
