647df26e1ca1502315f0a92666d6c9d6d9301c0e
ZacharyLawPep <zacharylaw@peplink.com>
2024-02-21 09:32:45 +0800

    nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,22 @@
-/**
-* @type {import('next').NextConfig}
-*/
-const nextConfig = {
-  images: {
-    loader: 'akamai',
-    path: '',
-  },
-  assetPrefix: './',
-};
+// next.config.js
 
-export default nextConfig;
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'the "domain" of your Imigix source',
+  },
+}
